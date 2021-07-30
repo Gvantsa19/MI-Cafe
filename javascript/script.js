8cb47c500006ac7b19589456f00eeb612d877990
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-30 20:39:29 +0400

    burger

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+let navBar = document.getElementById('nav-bar');
+let burgerButton = document.getElementById('burger');
+
+burgerButton.addEventListener('click', function () {
+    navBar.classList.toggle('active');
+})
