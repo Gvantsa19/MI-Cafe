2d3b70ad6bd90b0f934c3791f7694079f1745054
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-08-19 23:24:02 +0400

    update

--- javascript/links.js
+++ javascript/links.js
@@ -1,10 +1,12 @@
-let btnn = document.querySelectorAll('.linnk');
-let links = document.querySelectorAll('.links');
-
-document.querySelectorAll('.linnk').forEach((item) => {
+const links = document.querySelectorAll('.links-block');
+links.forEach((item, key) => {
     item.addEventListener('click', function() {
-        links.forEach((element) => {
-            element.classList.toggle('link-active');
+        item.classList.add('linnk');
+
+        links.forEach((el, index) => {
+            if(key !== index){
+                el.classList.remove('linnk');
+            }
         })
     })
 })
